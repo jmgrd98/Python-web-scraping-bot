0caec8ca97370fa7ac38a6cd5dc10af5e3507400
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-25 10:29:46 -0300

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,13 @@
+import os
 from selenium import webdriver
-url = 'https://www.foodstyles.com/'
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+url = 'http://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M'
+global browser
 browser = webdriver.Chrome()
 browser.get(url)
+
+
+input("Press enter to close the browser...")
