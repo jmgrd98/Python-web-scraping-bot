3eea2c8d3fad0e042f3bcfbe65f0377581009da1
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-26 00:10:03 -0300

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,19 @@
-import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 service = Service(ChromeDriverManager().install())
-url = 'http://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M'
-global browser
+
+urls = [
+    'https://www.corebyclaresmyth.com/home/menus/',
+    'https://hakkasan.com/mayfair/menu/',
+    'https://rivercafe.com/dinner-menu/'
+    ]
 browser = webdriver.Chrome()
-browser.get(url)
-browser.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys('fuck')
+
+for url in urls:
+    browser.get(url)
+    element = browser.find_element('xpath', '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/p[1]/strong')
+    print(element.text)
 
 input("Press enter to close the browser...")
