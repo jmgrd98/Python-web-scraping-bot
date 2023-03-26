de1a175bc72b7a684eec7d315e4349fd8641ed8f
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-26 10:39:43 -0300

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -7,8 +7,8 @@ service = Service(ChromeDriverManager().install())
 
 urls = [
     'https://www.corebyclaresmyth.com/home/menus/',
-    # 'https://hakkasan.com/mayfair/menu/',
-    # 'https://rivercafe.com/dinner-menu/'
+    'https://hakkasan.com/mayfair/menu/',
+    'https://rivercafe.com/dinner-menu/'
     ]
 
 itemCounter = 0
@@ -17,15 +17,23 @@ browser = webdriver.Chrome()
 
 for url in urls:
     browser.get(url)
-    title = browser.find_element('xpath', '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/h3')
-    print(title.text)
 
-    while itemCounter < 7:
-        item_xpath = '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/p[{}]'
-        item_xpath = item_xpath.format(itemCounter + 1)
+    if url == urls[0]:
+        restaurantTitle = browser.find_element('xpath', '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/h3')
+        print(restaurantTitle.text)
+
+        while itemCounter < 7:
+            item_xpath = '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/p[{}]'
+            item_xpath = item_xpath.format(itemCounter + 1)
             
-        item = browser.find_element('xpath', item_xpath)
-        itemCounter += 1
-        print(item.text)
+            item = browser.find_element('xpath', item_xpath)
+            itemCounter += 1
+            print(item.text)
+
+    if url == urls[1]:
+        restaurantTitle = browser.find_element('xpath', '//*[@id="menu-driver"]/header/a[2]/div')
+        print(restaurantTitle.text)
+        print('ENTROU AQUI')
+            
 
 input("Press enter to close the browser...")
