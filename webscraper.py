44b63fda069a12dcd9461b35fc5609fc275fe685
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-26 01:13:37 -0300

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -7,15 +7,25 @@ service = Service(ChromeDriverManager().install())
 
 urls = [
     'https://www.corebyclaresmyth.com/home/menus/',
-    'https://hakkasan.com/mayfair/menu/',
-    'https://rivercafe.com/dinner-menu/'
+    # 'https://hakkasan.com/mayfair/menu/',
+    # 'https://rivercafe.com/dinner-menu/'
     ]
+
+itemCounter = 0
 
 browser = webdriver.Chrome()
 
 for url in urls:
     browser.get(url)
-    element = browser.find_element('xpath', '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/p[1]/strong')
-    print(element.text)
+    title = browser.find_element('xpath', '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/h3')
+    print(title.text)
+
+    while itemCounter < 7:
+        item_xpath = '//*[@id="primary"]/div/div[1]/div/div[1]/div[1]/p[{}]'
+        item_xpath = item_xpath.format(itemCounter + 1)
+            
+        item = browser.find_element('xpath', item_xpath)
+        itemCounter += 1
+        print(item.text)
 
 input("Press enter to close the browser...")
